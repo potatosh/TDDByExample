ee5234a7745ee6a9fe32d469ebea6ebdf8bbd648
Shouhei Hayakawa <s-hayakawa@esm.co.jp>
2011-07-27 13:24:03 +0900

    Part 1 Chapter 2

--- src/Dollar.java
+++ src/Dollar.java
@@ -4,13 +4,11 @@ public class Dollar {
 	public int amount;
 
 	public Dollar(int amount) {
-		// TODO 自動生成されたコンストラクター・スタブ
 		this.amount = amount;
 	}
 
-	public void times(int mul) {
-		// TODO 自動生成されたメソッド・スタブ
-		amount = amount * mul;
+	public Dollar times(int mul) {
+		return new Dollar(amount * mul);
 	}
 
 }
--- src/DollarTest.java
+++ src/DollarTest.java
@@ -1,11 +1,18 @@
 import junit.framework.TestCase;
 
-
+/*
+ * まず、5ドルを2倍すると10ドルになるというストーリーがある
+ * ・クラスがない→作る
+ * ・関数や変数がない→実装する
+ * ・テストは通るが失敗する（10にならない）→とりあえずグリーンにするためにどっかで10をこじつける
+ * ・
+ */
 public class DollarTest extends TestCase {
 	public void testTimes() {
-		//まず、5ドルを2倍すると10ドルになる
-		Dollar dollar = new Dollar(5);		//5ドル
-		dollar.times(2);			//2倍
-		assertEquals(10, dollar.amount);	//値を取り出したら10になってるはず
+		Dollar dollar = new Dollar(5);		//5ドルを作る
+		Dollar product = dollar.times(2);			//2倍する
+		assertEquals(10, product.amount);	//値を取り出したら10になってるはず
+		product = dollar.times(3);			//3倍する
+		assertEquals(15, product.amount);	//値を取り出したら15になってるはず
 	}
 }
